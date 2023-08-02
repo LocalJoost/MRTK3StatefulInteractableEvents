a5f6ac201f95f86c54e522051eba1784f98e4588
Joost van Schaik <joostvanschaik@outlook.com>
2023-08-02 20:02:32 +0200

    Initial code checkin

---
+++ Assets/Application/Scripts/SingleShotController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class SingleShotController : MonoBehaviour
+{
+    [SerializeField]
+    private Color activatedColor = Color.green;
+
+    [SerializeField]
+    private float resetTime = 0.75f;
+
+    private Color originalColor;
+
+    private Material material;
+
+    private float timeActivated = float.MinValue;
+
+    public void ShowActivated()
+    {
+        timeActivated = Time.time;
+    }
+
+    private void Start()
+    {
+        material = GetComponent<Renderer>().material;
+        originalColor = material.color;
+    }
+
+    void Update()
+    {
+        var desiredColor = Time.time - timeActivated > resetTime ? 
+            originalColor : activatedColor;
+        if (material.color != desiredColor)
+        {
+            material.color = desiredColor;
+        }
+    }
+}
